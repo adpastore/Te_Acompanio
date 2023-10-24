e0479edc9e8d124b500e5e407befd2375b5b01f2
NicoBaires <nicolamattina80@gmail.com>
2023-10-23 23:38:14 -0300

    Pagina de contacto preliminar

---
+++ script.js
@@ -0,0 +1,31 @@
+// Validar completar campos
+// import swal from 'sweetalert';
+const form = document.getElementById('contact-form');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const email = document.getElementById('email').value;
+    const comentario = document.getElementById('comentario').value;
+
+    if (!nombre || !apellido || !email || !comentario) {
+        swal("Por favor, completa todos los campos.","","error");
+        return;
+    }
+
+    swal("Formulario enviado con éxito.", "Gracias", "success");
+});
+
+
+function formatoTelefono(input) {
+    // Elimina cualquier caracter que no sea un número
+    input.value = input.value.replace(/\D/g, '');
+
+    // Formatea el número con guiones
+    if (input.value.length >= 2 && input.value.length <= 6) {
+        input.value = input.value.substring(0, 2) + '-' + input.value.substring(2);
+    } else if (input.value.length > 6) {
+        input.value = input.value.substring(0, 2) + '-' + input.value.substring(2, 6) + '-' + input.value.substring(6);
+    }
+}
