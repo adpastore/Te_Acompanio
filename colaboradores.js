d3c06ddd02909362c75a33bbbeab9f52d9439bca
Andres Pastore <adpastore@gmail.com>
2023-10-29 00:37:57 -0300

    pagina de colaboradores y media querys

---
+++ colaboradores.js
@@ -0,0 +1,27 @@
+let contenido = document.querySelector('#contenido');
+let mainElement = document.querySelector('#main'); // Accede al elemento <main> por su id "main"
+
+function traerMultiples() {
+    fetch('https://randomuser.me/api/?results=6')
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            let contenidoHTML = '';
+
+            for (let i = 0; i < res.results.length; i++) {
+                contenidoHTML += `
+                    <div class="usuario">
+                        <img src="${res.results[i].picture.large}" width="100px" class="img-fluid rounded-circle">
+                        <h4>${res.results[i].name.first} ${res.results[i].name.last}</h4>
+                        <h6>${res.results[i].location.country}</h6>
+                        <a href="${res.results[i].email}" target="_blank">${res.results[i].email}</a>
+                    </div>
+                `;
+            }
+
+            mainElement.innerHTML = contenidoHTML; // Asigna el contenido HTML al elemento <main>
+        })
+        .catch(error => console.log("Ocurrió un error", error));
+}
+
+traerMultiples();
